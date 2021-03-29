e4a059b4a741dc32c80f9a1ffc98b6a7d7fcf15e
limgyumin <52942566+limgyumin@users.noreply.github.com>
2021-03-29 14:24:51 +0900

    FIX: Repository 패턴 -> Service에 Repository를 주입

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserRepository } from './user.repository';
+import { User } from './model/user.model';
 import { UserResolver } from './user.resolver';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [TypeOrmModule.forFeature([User])],
   exports: [UserService],
   providers: [UserService, UserResolver],
 })
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,85 +1,46 @@
-import {
-  BadRequestException,
-  ConflictException,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
-import { createToken } from 'src/lib/token';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateUserInput, UpdateUserInput } from './dto/user.input';
 import { User } from './model/user.model';
-import { UserRepository } from './user.repository';
 
 @Injectable()
 export class UserService {
-  constructor(private userRepository: UserRepository) {}
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
 
-  async registerUser(data: CreateUserInput): Promise<string> {
-    const isExist = await this.userRepository.findOneById(data.id);
-
-    if (isExist) {
-      throw new ConflictException('User already exist.');
-    }
-
+  async create(data: CreateUserInput): Promise<User> {
     const user = this.userRepository.create(data);
-
-    user.id = data.id;
-    user.password = data.password;
-
-    await this.userRepository.save(user);
-
-    return createToken(user);
+    return await user.save();
   }
 
-  async loginUser(id: string, password: string): Promise<string> {
-    const user = await this.userRepository.findOneById(id);
-
-    if (!user) {
-      throw new NotFoundException('User not found.');
-    }
-
-    const isMatch = password === user.password;
-
-    if (isMatch) {
-      return createToken(user);
-    } else {
-      throw new NotFoundException('Invalid password.');
-    }
-  }
-
-  async getAllUsers(page: number, limit: number): Promise<User[]> {
-    if (!page || !limit) {
-      throw new BadRequestException('Page or limit is null.');
-    }
-
-    if (page < 1) {
-      throw new BadRequestException('Page is invalid number');
-    }
-
-    const users = await this.userRepository.findAll(page, limit);
-
-    return users;
-  }
-
-  async updateUser(id: string, data: UpdateUserInput): Promise<User> {
-    const user = await this.userRepository.findOneById(id);
-
-    if (!user) {
-      throw new NotFoundException('User not found.');
-    }
-
+  async update(user: User, data: UpdateUserInput): Promise<User> {
     user.name = data.name !== undefined ? data.name : user.name;
     user.password = data.password !== undefined ? data.password : user.password;
+    user.bio = data.bio !== undefined ? data.bio : user.bio;
 
     return await this.userRepository.save(user);
   }
 
-  async deleteUser(id: string): Promise<User> {
-    const user = await this.userRepository.findOneById(id);
-
-    if (!user) {
-      throw new NotFoundException('User not found.');
-    }
-
+  async delete(user: User): Promise<User> {
     return await this.userRepository.remove(user);
+  }
+
+  findOneById(id: string): Promise<User> {
+    return this.userRepository
+      .createQueryBuilder()
+      .where('id = :id', { id })
+      .getOne();
+  }
+
+  findAll(page: number, limit: number): Promise<User[]> {
+    return this.userRepository
+      .createQueryBuilder()
+      .orderBy('created_at', 'ASC')
+      .skip((page - 1) * limit)
+      .take(limit)
+      .getMany();
   }
 }
