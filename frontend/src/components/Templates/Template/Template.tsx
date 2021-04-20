53dc6488ea3d8432c6772a3b35a9003e3a90a22e
limgyumin <52942566+limgyumin@users.noreply.github.com>
2021-04-20 13:39:46 +0900

    ADD: Template와 AuthTemplate로 분류

---
+++ frontend/src/components/Templates/Template/Template.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import classNames from "classnames";
+import { ClassNamesFn } from "classnames/types";
+
+type TemplateProps = {
+  children: React.ReactNode;
+};
+
+const styles = require("./Template.scss");
+const cx: ClassNamesFn = classNames.bind(styles);
+
+const Template = ({ children }: TemplateProps) => {
+  return (
+    <div className={cx("template")}>
+      <div className={cx("template-wrapper")}>{children}</div>
+    </div>
+  );
+};
+
+export default Template;
--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,7 +1,7 @@
 export { default as Main } from "./Main";
 export { default as SignIn } from "./SignIn";
 export { default as SignUp } from "./SignUp";
-export { default as InsertEmail } from "./InsertEmail";
-export { default as VerifyEmail } from "./VerifyEmail";
-export { default as GitHubAuth } from "./GitHubAuth";
-export { default as LocalAuth } from "./LocalAuth";
+export { default as InsertEmail } from "./Auth/InsertEmail";
+export { default as VerifyEmail } from "./Auth/VerifyEmail";
+export { default as GitHubAuth } from "./Auth/GitHubAuth";
+export { default as LocalAuth } from "./Auth/LocalAuth";
