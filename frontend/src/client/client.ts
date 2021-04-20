9687dd2ef69c50dc7d53eaa937c4128aa31da189
limgyumin <52942566+limgyumin@users.noreply.github.com>
2021-04-20 13:32:58 +0900

    FIX: ApolloLink에서 headers에 token을 삽입

--- frontend/src/client/client.ts
+++ frontend/src/client/client.ts
@@ -1,19 +1,33 @@
-import { InMemoryCache } from "apollo-cache-inmemory";
-import ApolloClient from "apollo-client";
-import { createHttpLink } from "apollo-link-http";
+import {
+  ApolloClient,
+  ApolloLink,
+  concat,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
 import { SERVER } from "config/config.json";
 import { getToken } from "lib/token";
 
-const httpLink = createHttpLink({
+const httpLink = new HttpLink({
   uri: SERVER,
   headers: {
     Authorization: `Bearer ${getToken()}`,
   },
 });
 
+const authMiddleWare = new ApolloLink((operation, forward) => {
+  operation.setContext({
+    headers: {
+      Authorization: `Bearer ${getToken()}`,
+    },
+  });
+
+  return forward(operation);
+});
+
 export const client = new ApolloClient({
-  link: httpLink,
   cache: new InMemoryCache({
     addTypename: false,
   }),
+  link: concat(authMiddleWare, httpLink),
 });
