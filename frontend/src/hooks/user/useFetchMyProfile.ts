6d6625981660a068c5e043bc1a104b1f2886feea
limgyumin <52942566+limgyumin@users.noreply.github.com>
2021-04-29 14:00:45 +0900

    FIX: token을 담을 변수를 useMemo로 관리

--- frontend/src/hooks/user/useFetchMyProfile.ts
+++ frontend/src/hooks/user/useFetchMyProfile.ts
@@ -2,23 +2,23 @@ import { useRecoilState } from "recoil";
 import { loginState, myProfileState } from "atom/auth.atom";
 import { getToken } from "lib/token";
 import { MY_PROFILE } from "graphql/user/user.query";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useMemo } from "react";
 import { useHistory } from "react-router";
 import { useLazyQuery } from "@apollo/client";
 import { IMyProfileResult } from "types/user/user.result.type";
-import { useToasts } from "react-toast-notifications";
 import { IUserShortInfo } from "types/user/user.type";
 
 const useFetchMyProfile = () => {
-  const { addToast } = useToasts();
   const [fetchMyProfile, { loading, data, error }] = useLazyQuery<
     IMyProfileResult
-  >(MY_PROFILE);
+  >(MY_PROFILE, { fetchPolicy: "no-cache" });
 
   const history = useHistory();
 
   const [profile, setProfile] = useRecoilState<IUserShortInfo>(myProfileState);
   const [login, setLogin] = useRecoilState<boolean>(loginState);
+
+  const token = useMemo(() => getToken(), []);
 
   const fetchResultHandler = useCallback((): void => {
     if (!loading && data) {
@@ -27,25 +27,22 @@ const useFetchMyProfile = () => {
     }
 
     if (error) {
-      addToast("질문 목록을 조회하는 중에 알 수 없는 오류가 발생했어요...", {
-        appearance: "error",
-      });
       history.push("/");
     }
-  }, [history, loading, data, error, setLogin, setProfile, addToast]);
+  }, [history, loading, data, error, setLogin, setProfile]);
 
   useEffect(() => {
-    const token = getToken();
     if (!login && token) {
       fetchMyProfile();
     }
-  }, [login, fetchMyProfile]);
+  }, [token, login, fetchMyProfile]);
 
   useEffect(() => {
     fetchResultHandler();
   }, [fetchResultHandler]);
 
   return {
+    token,
     login,
     loading,
     profile,
