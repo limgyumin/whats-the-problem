5c1505b32be2b3167d715b118fd5d5bb34058ee9
limgyumin <52942566+limgyumin@users.noreply.github.com>
2021-06-08 19:29:04 +0900

    FIX: 로딩 컴포넌트가 리스트 아래에 나오도록 수정, Template를 Main에서 사용

--- frontend/src/components/Main/MainQuestions/MainQuestions.tsx
+++ frontend/src/components/Main/MainQuestions/MainQuestions.tsx
@@ -3,9 +3,8 @@ import classNames from "classnames";
 import { ClassNamesFn } from "classnames/types";
 import useFetchQuestions from "hooks/question/useFetchQuestions";
 import MainQuestionItem from "./MainQuestionItem";
-import { MdQuestionAnswer } from "react-icons/md";
+import { RiQuestionAnswerLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
-import Template from "components/Templates/Template";
 import MainQuestionLoading from "./MainQuestionLoading";
 
 const styles = require("./MainQuestions.scss");
@@ -15,67 +14,65 @@ const MainQuestions = () => {
   const { isRecent, loading, questions, lastElRef } = useFetchQuestions();
 
   return (
-    <Template>
-      <div className={cx("main-questions")}>
-        <div className={cx("main-questions-header")}>
-          <MdQuestionAnswer className={cx("main-questions-header-icon")} />
-          <h1 className={cx("main-questions-header-title")}>질문 목록</h1>
-        </div>
-        <div className={cx("main-questions-sort")}>
-          <div className={cx("main-questions-sort-wrapper")}>
-            <Link to="/" className={cx("main-questions-sort-wrapper-recent")}>
-              <p
-                className={cx("main-questions-sort-wrapper-recent-text", {
-                  active: isRecent,
-                })}
-              >
-                최근 순
-              </p>
-            </Link>
-            <Link
-              to="/?sort=answer"
-              className={cx("main-questions-sort-wrapper-answer")}
-            >
-              <p
-                className={cx("main-questions-sort-wrapper-answer-text", {
-                  active: !isRecent,
-                })}
-              >
-                답변 순
-              </p>
-            </Link>
-            <div
-              className={cx("main-questions-sort-wrapper-bottom", {
-                move: !isRecent,
+    <div className={cx("main-questions")}>
+      <div className={cx("main-questions-header")}>
+        <RiQuestionAnswerLine className={cx("main-questions-header-icon")} />
+        <h1 className={cx("main-questions-header-title")}>질문 목록</h1>
+      </div>
+      <div className={cx("main-questions-sort")}>
+        <div className={cx("main-questions-sort-wrapper")}>
+          <Link to="/" className={cx("main-questions-sort-wrapper-recent")}>
+            <p
+              className={cx("main-questions-sort-wrapper-recent-text", {
+                active: isRecent,
               })}
-            />
-          </div>
-        </div>
-        <div className={cx("main-questions-list")}>
-          {loading && (
-            <React.Fragment>
-              <MainQuestionLoading />
-              <MainQuestionLoading />
-              <MainQuestionLoading />
-              <MainQuestionLoading />
-              <MainQuestionLoading />
-              <MainQuestionLoading />
-            </React.Fragment>
-          )}
-          {questions.map((question, idx) =>
-            questions.length - 1 === idx ? (
-              <MainQuestionItem
-                key={question.idx}
-                lastElRef={lastElRef}
-                question={question}
-              />
-            ) : (
-              <MainQuestionItem key={idx} question={question} />
-            )
-          )}
+            >
+              최근 순
+            </p>
+          </Link>
+          <Link
+            to="/?sort=answer"
+            className={cx("main-questions-sort-wrapper-answer")}
+          >
+            <p
+              className={cx("main-questions-sort-wrapper-answer-text", {
+                active: !isRecent,
+              })}
+            >
+              답변 순
+            </p>
+          </Link>
+          <div
+            className={cx("main-questions-sort-wrapper-bottom", {
+              move: !isRecent,
+            })}
+          />
         </div>
       </div>
-    </Template>
+      <div className={cx("main-questions-list")}>
+        {questions.map((question, idx) =>
+          questions.length - 1 === idx ? (
+            <MainQuestionItem
+              key={question.idx}
+              lastElRef={lastElRef}
+              question={question}
+            />
+          ) : (
+            <MainQuestionItem key={question.idx} question={question} />
+          )
+        )}
+        {loading && (
+          <React.Fragment>
+            <MainQuestionLoading />
+            <MainQuestionLoading />
+            <MainQuestionLoading />
+            <MainQuestionLoading />
+            <MainQuestionLoading />
+            <MainQuestionLoading />
+          </React.Fragment>
+        )}
+      </div>
+    </div>
   );
 };
 
