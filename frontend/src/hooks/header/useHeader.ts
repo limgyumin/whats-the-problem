52dbd1adcd78b2e97a4aa9519eed42da9e69deb5
limgyumin <52942566+limgyumin@users.noreply.github.com>
2021-06-08 19:56:51 +0900

    FIX: useHeaderMenu와 useHeader Hooks를 통합

--- frontend/src/hooks/header/useHeader.ts
+++ frontend/src/hooks/header/useHeader.ts
@@ -1,16 +1,39 @@
+import { useCallback, useRef } from "react";
+import { useRecoilState, useSetRecoilState } from "recoil";
+import { loginState, myProfileState } from "atom/auth.atom";
 import { showMenuState } from "atom/header.atom";
-import { useRecoilState } from "recoil";
+import { removeCookie } from "lib/cookie";
+import { initialUserShortInfo } from "types/user/user.initial-state";
+import { IUserShortInfo } from "types/user/user.type";
+import useClose from "hooks/util/useClose";
 
 const useHeader = () => {
   const [show, setShow] = useRecoilState<boolean>(showMenuState);
 
-  const showMenuHandler = (state: boolean) => {
-    setShow(state);
-  };
+  const clickRef = useRef<HTMLDivElement>(null);
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  const setProfile = useSetRecoilState<IUserShortInfo>(myProfileState);
+  const setLogin = useSetRecoilState<boolean>(loginState);
+
+  const logoutHandler = useCallback(() => {
+    removeCookie("token");
+    setProfile(initialUserShortInfo);
+    setLogin(false);
+  }, [setProfile, setLogin]);
+
+  const showMenuHandler = useCallback(() => {
+    setShow(!show);
+  }, [show, setShow]);
+
+  useClose<HTMLDivElement>(clickRef, menuRef, showMenuHandler);
 
   return {
+    clickRef,
+    menuRef,
     show,
     showMenuHandler,
+    logoutHandler,
   };
 };
 
